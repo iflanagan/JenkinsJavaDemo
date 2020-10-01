494ffca6c02a11a845a01f222993e2cec114b959
Ian Flanagan <ianianf@yahoo.com>
2020-10-01 14:58:26 -0500

    Fixed issues.

---
+++ src/main/java/com/IanFlanagan/Utils.java
@@ -0,0 +1,48 @@
+package com.IanFlanagan;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+
+import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
+
+public class Utils {
+
+    private static Rollbar rollbar;
+    private static String accessToken;
+    private static String env;
+    private static String version;
+
+    public static Rollbar createRBinstanceConfig(String accessToken, String environment, String version) {
+
+
+        if (accessToken == null) {
+            System.out.printf("Please enter a valid rollbar Access Token");
+            return null;
+        }
+
+        try
+        {
+            Rollbar rollbar = Rollbar.init(withAccessToken(System.getenv("ROLLBAR_ACCESS_TOKEN"))
+                    .environment(System.getenv("ENVIRONMENT"))
+                    .codeVersion(System.getenv("CODE_VERSION"))
+                    .person(new MyPersonProvider())
+                    .server(new ServerProvider())
+                    .build());
+
+
+        } catch (Exception e) {
+
+            System.out.printf("can't create rollbar instance: " +e.getMessage());
+            e.printStackTrace();
+            rollbar = null;
+        }
+
+       /* Config config = withAccessToken(accessToken)
+                .environment(environment)
+                .codeVersion(version)
+                .build();
+        rollbar = Rollbar.init(config);
+        return rollbar;*/
+        return rollbar;
+    }
+}
--- src/test/java/com/IanFlanagan/ApplicationTest.java
+++ src/test/java/com/IanFlanagan/ApplicationTest.java
@@ -1,6 +1,8 @@
 package com.IanFlanagan;
 
 import com.rollbar.api.payload.data.Level;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import com.rollbar.notifier.Rollbar;
 
@@ -12,19 +14,14 @@ import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
 public class ApplicationTest {
 
-    private Rollbar rollbar;
+    private static Rollbar rollbar;
     private static Map<String, Object> myRollbarMap = new HashMap<>();
     private static Level WARNING;
     private static Level SEVERE;
     private static Level INFO;
     private static Level CONFIG;
 
-    @Test
-    public void generateData() throws Exception {
-
-        System.out.println("Starting rollbar test to generate Data");
-
-        /*
+      /*
         TO obtain the Rollbar Access token follow these steps
         Login to Rollbar.com
         Click on settings
@@ -33,6 +30,10 @@ public class ApplicationTest {
         Copy the token next to where it says 'post_server_item'
          */
 
+    @Test
+    public void createErrorsRB() throws Exception {
+
+        System.out.println("Starting rollbar test to generate Data");
         Rollbar rollbar = Rollbar.init(withAccessToken(System.getenv("ROLLBAR_ACCESS_TOKEN"))
                 .environment(System.getenv("ENVIRONMENT"))
                 .codeVersion(System.getenv("CODE_VERSION"))
@@ -40,12 +41,14 @@ public class ApplicationTest {
                 .server(new ServerProvider())
                 .build());
 
+      //  Rollbar rollbar = Utils.createRBinstanceConfig(System.getenv("ROLLBAR_ACCESS_TOKEN"),System.getenv("ENVIRONMENT"),System.getenv("CODE_VERSION"));
+
         try
         {
             throw new RuntimeException("New Runtime exception");
         } catch (Exception e) {
 
-            for (int i=0;i<=50; i++ ) {
+            for (int i=0;i<=5; i++ ) {
 
                 rollbar.critical(e, " Ian test: now");
                 rollbar.log("Hello, Rollbar this is message number: " +i+ " in case you were wondering");
@@ -63,14 +66,47 @@ public class ApplicationTest {
 
             rollbar.close(true);
         }
-     //   rollbar.log("Hello, Rollbar");
-
-       // Application.GenerateErrors();
 
 
-       /*
-        Application app = new Application();
-        app.execute();*/
+
+    }
+
+    @Test
+    public void generateData() throws Exception {
+
+      //  Rollbar rollbar = Utils.createRBinstanceConfig(System.getenv("ROLLBAR_ACCESS_TOKEN"),System.getenv("ENVIRONMENT"),System.getenv("CODE_VERSION"));
+
+        System.out.println("Starting rollbar test to generate Data");
+        Rollbar rollbar = Rollbar.init(withAccessToken(System.getenv("ROLLBAR_ACCESS_TOKEN"))
+                .environment(System.getenv("ENVIRONMENT"))
+                .codeVersion(System.getenv("CODE_VERSION"))
+                .person(new MyPersonProvider())
+                .server(new ServerProvider())
+                .build());
+
+        try
+        {
+            throw new RuntimeException("New Runtime exception");
+        } catch (Exception e) {
+
+            for (int i=0;i<=5; i++ ) {
+
+                rollbar.critical(e, " Ian test: now");
+                rollbar.log("Hello, Rollbar this is message number: " +i+ " in case you were wondering");
+                rollbar.critical(e, " Ian test: " +i+ " now");
+                rollbar.info("Theres a problem");
+                rollbar.warning(e);
+                rollbar.log(e,"Error log");
+                rollbar.error(e);
+                rollbar.critical(e,myRollbarMap, "Custom Field Test");
+                rollbar.log(e,"RuntimeException");
+                rollbar.log(e, myRollbarMap, "Big time issue", SEVERE, true);
+
+            }
+        } finally {
+
+            rollbar.close(true);
+        }
 
     }
 
@@ -78,5 +114,35 @@ public class ApplicationTest {
     public void printName() {
         System.out.println("calling printName method test");
     }
+
+   /* @Before
+    public void setup() {
+
+        try
+        {
+            System.out.println("Starting rollbar test to generate Data");
+            Rollbar rollbar = Rollbar.init(withAccessToken(System.getenv("ROLLBAR_ACCESS_TOKEN"))
+                    .environment(System.getenv("ENVIRONMENT"))
+                    .codeVersion(System.getenv("CODE_VERSION"))
+                    .person(new MyPersonProvider())
+                    .server(new ServerProvider())
+                    .build());
+
+        } catch (Exception e) {
+            System.out.printf("problem with before method:  " +e.getMessage());
+            e.printStackTrace();
+        }
+    }*/
+   /* @After
+    public void teardown() throws Exception {
+
+        try
+        {
+            rollbar.close(true);
+        } catch (Exception e) {
+            System.out.printf("problem with tear down " +e.getMessage());
+            e.printStackTrace();
+        }
+    }*/
 
 }
